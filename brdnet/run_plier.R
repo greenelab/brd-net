2c35526d1e960a1c014f44c1caee4e742733360f
Ben <ben.jer.heil@gmail.com>
2019-07-24 15:20:58 -0400

    Added a script to run PLIER on the output of download_categorized_data.ipynb

---
+++ brdnet/run_plier.R
@@ -0,0 +1,47 @@
+# This script runs PLIER on the data from download_categorized_data.ipynb
+
+# CRAN dependencies
+library('dplyr')
+
+# Bioconductor dependencies
+library('biomaRt')
+
+# Github dependencies
+library('PLIER')
+
+data("canonicalPathways")
+data("bloodCellMarkersIRISDMAP")
+data("svmMarkers")
+
+data.dir <- '../data'
+
+# Set the random seed
+set.seed(42)
+
+# Load and combine data from download_categorized_data.ipynb
+disease.df <- read.table(file.path(data.dir, 'plier_disease.tsv'), sep = '\t', stringsAsFactors = FALSE, header = TRUE)
+healthy.df <- read.table(file.path(data.dir, 'plier_healthy.tsv'), sep = '\t', stringsAsFactors = FALSE, header = TRUE)
+
+######MERGE BY ROWNAMES/INDEX########
+all.df <- merge.data.frame(healthy.df, disease.df, by='row.names')
+
+row.names(all.df) <- all.df['Row.names'][[1]]
+all.df['Row.names'] <- NULL
+
+allPaths <- combinePaths(bloodCellMarkersIRISDMAP, canonicalPathways, svmMarkers)
+
+all.matrix <- as.matrix(all.df)
+
+# This portion is based on the function PLIERNewData from  
+# https://github.com/greenelab/multi-plier/blob/master/util/plier_util.R 
+# Keep only genes found in both the id matrix and the prior knowlege gene sets
+common.genes <- PLIER::commonRows(all.matrix, allPaths)
+row.normalized <- PLIER::rowNorm(all.matrix)
+
+# Estimate the value of k (number of  pcs) to be used in PLIER
+k.estimate <- PLIER::num.pc(row.normalized[common.genes, ])
+
+plier.results <- PLIER(row.normalized[common.genes, ], allPaths, k=k.estimate)
+
+# Save PLIER loadings
+write.table(plier.results$Z, file='../data/plier_Z.tsv', sep='\t', row.names=TRUE, col.names = NA)
