474c56195fb820a7f82d544c8d1bffedb275f038
Ben Heil <ben.jer.heil@gmail.com>
2019-08-01 11:58:13 -0400

    Pull out validation set based on studies, not samples

--- brdnet/classifier.py
+++ brdnet/classifier.py
@@ -1,7 +1,9 @@
 '''Train models to differentiate between case and control gene expression data'''
 
 import argparse
+from collections import Counter
 import inspect
+import random
 import sys
 import time
 import os
@@ -40,7 +42,7 @@ def validate_model_name(model_name):
         sys.exit()
 
 
-def get_model(model_name, logdir):
+def get_model(model_name, logdir, lr):
     '''Retrieve a Model object from the models.py module by name
 
     Arguments
@@ -49,6 +51,8 @@ def get_model(model_name, logdir):
         The name of the model to retrieve
     logdir: string
         The path to the directory to save logs to
+    lr: float
+        The learning rate to be used by the optimizer
 
     Returns
     -------
@@ -60,7 +64,7 @@ def get_model(model_name, logdir):
 
     modelInstance = model()
 
-    optimizer = tf.keras.optimizers.Adam(lr=1e-6)
+    optimizer = tf.keras.optimizers.Adam(lr=lr)
 
     modelInstance.compile(optimizer=optimizer,
                           loss='binary_crossentropy',
@@ -98,6 +102,56 @@ def reduce_dimensionality(expression_df, Z_df):
     return reduced_matrix
 
 
+def get_validation_set(data, validation_fraction=.2):
+    '''Split a dataframe into training and validation data by extracting studies
+       that contain a certain fraction of the samples
+
+       Arguments
+       ---------
+       data: pandas.DataFrame
+           data is a dataframe where the rows represent genes and columns represent samples.
+           The column names should be of the format 'studyid.runid' to allow the study
+           information to be used.
+       validation_fraction: float
+           The fraction of the dataset to be pulled out as validation data
+
+       Returns
+       -------
+       train_df: pandas.DataFrame
+           A dataframe containing the fraction of the sample to be used in training
+       val_df:
+           A dataframe containing the fraction of the sample to be used for validation
+    '''
+    studies = [name.split('.')[0] for name in data.columns]
+    # random.shuffle shuffles in place
+    random.shuffle(studies)
+
+    counter = Counter(studies)
+
+    target_sample_count = len(data.columns) * validation_fraction
+
+    samples_so_far = 0
+    val_studies = []
+    for study, samples in counter.items():
+        # Prevent the validation set from being too much larger than the target
+        if samples_so_far + samples > len(data.columns) * (validation_fraction + .05):
+            continue
+        val_studies.append(study)
+        samples_so_far += samples
+
+        if samples_so_far >= target_sample_count:
+            break
+
+    val_columns = []
+    for study in val_studies:
+        val_columns.extend([col for col in data.columns if study in col])
+
+    val_df = data[val_columns]
+    train_df = data[data.columns.difference(val_columns)]
+
+    return train_df, val_df
+
+
 def load_data(args):
     '''Load and process the training data
 
@@ -120,6 +174,11 @@ def load_data(args):
     Z_df = Z_df.sort_index()
 
     healthy_df = pandas.read_csv(args.healthy_file_path, sep='\t')
+
+    healthy_train, healthy_val = get_validation_set(healthy_df)
+    sys.exit(0)
+
+    #TODO reduce dimensionality of both train and val dfs
     healthy_matrix = reduce_dimensionality(healthy_df, Z_df)
 
     disease_df = pandas.read_csv(args.disease_file_path, sep='\t')
@@ -130,9 +189,6 @@ def load_data(args):
 
     train_X = numpy.concatenate([healthy_matrix, disease_matrix])
     train_Y = numpy.concatenate([healthy_labels, disease_labels])
-
-    #TODO keep metadata with training data somehow
-    # (https://github.com/greenelab/brd-net/pull/7#discussion_r309284156)
 
     return train_X, train_Y
 
@@ -155,6 +211,11 @@ if __name__ == '__main__':
     parser.add_argument('--model', help='The name of the model to be used. The models currently '
                         'available are: {}'.format(', '.join(model_list)), default='MLP')
     parser.add_argument('-s', '--seed', help='The seed to be used in random number generators', default=42)
+    parser.add_argument('-l', '--learning_rate', help='The learning rate to be used by the '
+                                                      'optimization process',
+                        default=1e-5)
+    parser.add_argument('--epochs', help='The maximum number of epochs to train the model for',
+                        default=1000)
 
     args = parser.parse_args()
 
@@ -170,11 +231,12 @@ if __name__ == '__main__':
 
     train_X, train_Y = load_data(args)
 
-    model = get_model(model_name, args.logdir)
+    model = get_model(model_name, args.logdir, args.learning_rate)
 
     model.fit(train_X, train_Y,
             batch_size=16,
-            epochs=1000,
+            epochs=args.epochs,
             validation_split=.2,
-            callbacks=[tf.keras.callbacks.TensorBoard(log_dir=args.logdir)]
-            )
+            callbacks=[tf.keras.callbacks.TensorBoard(log_dir=args.logdir),
+                       tf.keras.callbacks.ReduceLROnPlateau(min_lr=1e-11),
+                      ])
