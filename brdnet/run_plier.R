574a6b6c709de4afdf766f62325c880240a3fb96
Ben Heil <ben.jer.heil@gmail.com>
2019-08-14 14:01:44 -0400

    Add command line parameters for output dir and k to run_plier

--- brdnet/run_plier.R
+++ brdnet/run_plier.R
@@ -10,12 +10,31 @@ library('biomaRt')
 # Install PLIER if it isn't already present
 if (!require('PLIER',character.only = TRUE)) {
 	library('devtools')
-	install_github('wgmao/PLIER')
+	tryCatch({
+		install_github('wgmao/PLIER')
+	},
+	error=function(e){
+		# For some reason devtools struggles to find the tar installation, this should fix it
+		Sys.setenv(TAR = "/bin/tar")
+		install_github('wgmao/PLIER')
+	})
+
 }
 library('PLIER')
 
-
 data.dir <- '../data'
+
+args <- commandArgs(trailingOnly=TRUE)
+
+if(length(args) < 2){
+	print('As no value of k was passed in, it will be estimated from the data')
+} else if (length(args) > 2){
+	print('Usage: Rscript run_plier.R outdir [k]')
+	quit(status=1)
+} else if (length(args) == 0){
+	print('Usage: Rscript run_plier.R outdir [k]')
+	quit(status=1)
+}
 
 # Set the random seed
 set.seed(42)
@@ -44,10 +63,16 @@ all.matrix <- as.matrix(all.df)
 common.genes <- PLIER::commonRows(all.matrix, allPaths)
 row.normalized <- PLIER::rowNorm(all.matrix)
 
-# Estimate the value of k (number of  pcs) to be used in PLIER
-k.estimate <- PLIER::num.pc(row.normalized[common.genes, ])
+k.estimate = NaN
+out.dir <- args[1]
+if (length(args) == 1){
+	k.estimate <- PLIER::num.pc(row.normalized[common.genes, ])
+} else if (length(args) == 2){
+	k.estimate <- as.numeric(args[2])
+}
+
 
 plier.results <- PLIER(row.normalized[common.genes, ], allPaths, k=k.estimate)
 
 # Save PLIER loadings
-write.table(plier.results$Z, file='../data/plier_Z.tsv', sep='\t', row.names=TRUE)
+write.table(plier.results$Z, file=file.path(out.dir, paste('plier', k.estimate, 'Z.tsv', sep='_')), sep='\t', row.names=TRUE)
